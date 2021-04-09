4010fce146b45b0fb7211e2385b1c57dba0f0255
NanMed <46913776+NanMed@users.noreply.github.com>
2021-04-09 12:50:41 -0500

    App libros

---
+++ app/src/main/java/edu/itesm/nytimes/MainActivity.kt
@@ -0,0 +1,94 @@
+package edu.itesm.nytimes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+    private val BASE_URL = "https://api.nytimes.com/svc/books/v3/lists/current/"
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var manager: RecyclerView.LayoutManager
+    private lateinit var myAdapter: RecyclerView.Adapter<*>
+    private lateinit var results: Results
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        manager = LinearLayoutManager(this)
+        getAllData()
+        Toast.makeText(applicationContext, "En OnCreate", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(applicationContext, "En OnStart", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext, "En OnResume", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(applicationContext, "En OnStop", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(applicationContext, "En OnPause", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext, "En OnDestroy", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(applicationContext, "En OnRestart", Toast.LENGTH_LONG).show()
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
+    fun getAllData(){
+
+        val callToService = getRetrofit().create(APIService::class.java)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val responseFromService = callToService.getBooks()
+            runOnUiThread {
+                results = responseFromService.body() as Results
+
+                if (responseFromService.isSuccessful) {
+                    Log.i("Books", results.results?.books.toString())
+
+                    recyclerView = findViewById<RecyclerView>(R.id.recycler_view).apply {
+                        /*
+                        * Completa el código y crea el adapter.
+                        * */
+                        myAdapter = BooksAdapter(results.results?.books)
+                        layoutManager = manager
+                        adapter = myAdapter
+                    }
+
+                } else {
+                    Toast.makeText(applicationContext, "Error!", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+}
