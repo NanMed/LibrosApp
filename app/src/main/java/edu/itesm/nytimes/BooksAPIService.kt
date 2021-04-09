895054c08c2c9dfe1bb5ca535727c06102f8dd66
NanMed <46913776+NanMed@users.noreply.github.com>
2021-04-09 12:51:39 -0500

    Key Generica

--- app/src/main/java/edu/itesm/nytimes/BooksAPIService.kt
+++ app/src/main/java/edu/itesm/nytimes/BooksAPIService.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 
 interface APIService {
-    @GET("hardcover-fiction.json?api-key=E8MIDB9NM3B8VEdXSlsKEKRsdAdraODV")
+    @GET("hardcover-fiction.json?api-key=YOURKEY")
     suspend fun getBooks() : Response<Results>
 }
